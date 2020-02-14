ae021f8deb71ec9b0bdc3c7f08f61cbcc80063e9
Simone Turco <simone.turco@charite.de>
2020-02-14 18:07:12 +0100

    margin top for the search bar

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
-    <div className="ui container">
+    <div className="ui container" style={{ marginTop: "10px" }}>
       <SearchBar />
     </div>
   );
