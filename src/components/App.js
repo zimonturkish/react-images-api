b2b1422ed5a99b9197ef3e6774aa5d7f6268a8e6
Simone Turco <simone.turco@charite.de>
2020-02-17 17:07:02 +0100

    invoking callback onSubmit in Searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
