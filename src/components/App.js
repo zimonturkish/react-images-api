61b4b05fbd37efc74e358da2be83697f7ecd0fa3
Simone Turco <simone.turco@charite.de>
2020-02-17 20:09:33 +0100

    creating/importing ImageList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
