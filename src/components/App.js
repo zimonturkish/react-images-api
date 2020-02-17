b7ac343c501e7a14a586497c606e664cece8a958
Simone Turco <simone.turco@charite.de>
2020-02-17 18:44:04 +0100

    response

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,16 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: "Client-ID KE9QUrPyp-5Hc5GXE7Pf2E6vr_KDaf83qTLxoCovGGA"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization: "Client-ID KE9QUrPyp-5Hc5GXE7Pf2E6vr_KDaf83qTLxoCovGGA"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      });
   }
 
   render() {
