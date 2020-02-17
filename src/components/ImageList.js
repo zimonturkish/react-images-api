97fe2eff608e011cb384aaa368507db546bb35b9
Simone Turco <simone.turco@charite.de>
2020-02-17 21:09:32 +0100

    prop for alt in img

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return <img key={image.id} src={image.urls.regular} />;
+    return (
+      <img key={image.id} src={image.urls.regular} alt={image.description} />
+    );
   });
 
   return <div>{images}</div>;
