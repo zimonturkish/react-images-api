c5f9b5cc3c2497d1ecde1f7c18c743bd02ba06cc
Simone Turco <simone.turco@charite.de>
2020-02-17 20:04:54 +0100

    importing unsplash in App

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-axios.create({
+export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization: "Client-ID KE9QUrPyp-5Hc5GXE7Pf2E6vr_KDaf83qTLxoCovGGA"
   }
 });
-
-export default unsplash;
