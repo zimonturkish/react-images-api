592e08772a7e4d929f308bb8f3322aa00a1e4420
Simone Turco <simone.turco@charite.de>
2020-02-17 21:04:05 +0100

    id property in img

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+    return <img key={image.id} src={image.urls.regular} />;
   });
 
   return <div>{images}</div>;
