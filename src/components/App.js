938c68e63af0c7c3cb9e4cad96f2207fbb27b6c1
Simone Turco <simone.turco@charite.de>
2020-02-17 19:16:53 +0100

    message number found images

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
