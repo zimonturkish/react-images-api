0a3116091976941dad59e4780e27bc15c3c82cbe
Turco <turcos@charite.de>
2020-02-18 13:23:02 +0100

    Updates

---
+++ precache-manifest.b95cd8685a7111cf87999d3208fae64f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f48b44b88728fdc742e3681d9c6a856",
+    "url": "/react-images-api/index.html"
+  },
+  {
+    "revision": "4070e4bd81f462529155",
+    "url": "/react-images-api/static/css/main.5678862c.chunk.css"
+  },
+  {
+    "revision": "de032f242a06b52b3b1d",
+    "url": "/react-images-api/static/js/2.6a6949bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-images-api/static/js/2.6a6949bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4070e4bd81f462529155",
+    "url": "/react-images-api/static/js/main.f626eaa9.chunk.js"
+  },
+  {
+    "revision": "39bb1e4d96c668e82133",
+    "url": "/react-images-api/static/js/runtime-main.9fc05228.js"
+  }
+]);
