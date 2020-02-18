ac100cef0ff3d94c794afa62e40b4e7118bb6bb5
Simone Turco <simone.turco@charite.de>
2020-02-18 09:36:54 +0100

    new ImageList.css + import in ImageList.js

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = props => {
   const images = props.images.map(({ description, id, urls }) => {
     return <img key={id} src={urls.regular} alt={description} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
